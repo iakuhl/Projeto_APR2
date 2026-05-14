b065059c2570e69129c0709153c5c67cbdfe400d
iakuhl <ianodark@gmail.com>
2026-05-14 19:21:03 -0300

    Add defines for constants in defines.h

---
+++ PROJETO/defines.h
@@ -0,0 +1,12 @@
+// Defines
+
+
+
+#define TAM_CPF 12
+#define TAM_TELEFONE 20
+#define TAM_NACIONALIDADE 30
+#define TAM_TEXTO_PEQUENO 50
+#define TAM_TEXTO_GRANDE 200
+#define TAM_BUFFER_LEITURA 64 // Utilizada para leitura de inteiros, garantindo que o buffer seja grande o suficiente para evitar estouro e permitindo a validação adequada da entrada do usuário.
+#define MSG_LOOP_INFINITO "Detectamos um ERRO IRRECUPERÁVEL! Encerrando programa...\n" // Utilizada nos menus para informar o usuário sobre a ocorrência de um erro de leitura (EOF ou erro de entrada).
+#define MSG_ENTRADA_INVALIDA "Entrada inválida! Tente novamente.\n" // Utilizada nos menus para informar o usuário sobre a entrada de dados inválida (fora do intervalo permitido ou tipo de dado incorreto).

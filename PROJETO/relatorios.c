c59080ec393d43c168d87898b503a6005131d848
iakuhl <ianodark@gmail.com>
2026-05-15 21:11:19 -0300

    Add menu for reports in relatorios.c

---
+++ PROJETO/relatorios.c
@@ -0,0 +1,12 @@
+
+
+static int menuRelatorios()
+{
+    printf("### MENU RELATORIOS ###\n");
+    printf("1 - Relatorio de Artistas\n");
+    printf("2 - Relatorio de Obras\n");
+    printf("3 - Relatorio de Colaboracoes\n");
+    printf("4 - Voltar ao Menu Principal\n");
+
+    return escolherOpcao(1, 4);
+}
